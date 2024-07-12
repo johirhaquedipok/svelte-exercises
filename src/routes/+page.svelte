c588e75583617aaa599ecb69500d189dce439fae
johirhaqedipok <johirhaquedipok2@gmail.com>
2024-07-13 04:11:42 +0600

    feat: layout skeleton & input handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,141 +1,69 @@
 <script lang="ts">
+  import Navbar from "../components/navbar.svelte";
+  import Sidebar from "../components/sidebar.svelte";
+
 
 	type Todo = {
-		text: string;
-		done: boolean;
+		id: number;
+		text: string,
+		status: boolean
 	}
+	let todos = $state<Todo[]>([]);
 
-	type Filters = 'all' | 'active' | 'completed'
+	let inputError = $state<string>("")
 
-	let todos = $state<Todo[]>([])
-	let filter = $state<Filters>('all')
+	function addTodo (event:KeyboardEvent) {
+		if(event.key !== 'Enter') return;
 
-	let filteredTodos = $derived(filterTodos())
-
-	function addTodo(event:KeyboardEvent) {
-		if(event.key !== 'Enter') return ;
-
+		
 		const todoEl = event.target as HTMLInputElement;
-		// const id = window.crypto.randomUUID();
-		const text = todoEl.value ;
-		const done = false;
+		const text = todoEl.value;
 
-		todos = [...todos,{text, done}]
-		
+	if(!text) {
+		inputError = 'Input field can not be empty';
+		return;
+	}
+
+		todos = [...todos, {text, id:todos.length + 1, done: false}];
 		todoEl.value = "";
+		inputError = "";
 	}
-
-
-
-	function editTodo(event:Event) {
-		const inputEl = event.target as HTMLInputElement;
-		const index = inputEl.dataset.index;
-		todos[index].text = inputEl.value;
-	}
-
-	function toggleTodo(event:Event) {
-		const inputEl = event.target as HTMLInputElement;
-		const index = inputEl.dataset.index;
-		todos[index].done = !todos[index].done;
-	}
-
-	function setFilter(newFilter:Filters) {
-		filter = newFilter
-	}
-
-
-	function filterTodos() {
-		switch (filter) {
-			case 'all':
-				
-				return todos;
-				case 'active':
-				
-				return todos?.filter(todo => !todo?.done)
-				case 'completed':
-				
-				return todos?.filter(todo => !todo?.done)
-		
-			default:
-				return todos;
-		}
-	}
-
-	function remaining () {
-		return todos.filter(todo => !todo?.done)?.length
-	}
-
-	$effect(() =>{
-		const savedTodos = localStorage.getItem('todos');
-		if (savedTodos) {
-			todos = JSON.parse(savedTodos);
-		}
-	})
-
-	$effect(() => {
-		localStorage.setItem('todos', JSON.stringify(todos))
-	})
 </script>
+<div class=" m-auto max-h-screen h-screen bg-white shadow dark:bg-gray-800 dark:text-white">
+	<Navbar />
+	<div class="grid grid-cols-12 h-full gap-4">
+		<Sidebar />
+		<!-- main section starts -->
+		<div class="mt-4 col-span-10">
+			<section class="relative w-full max-w-md px-5 py-4 mx-auto rounded-md">
+				<div class="relative">
+					<span class="absolute inset-y-0 left-0 flex items-center pl-3">
+						
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-gray-400">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+						  </svg>
+						  
+					</span>
+			
+					<input onkeydown={addTodo} type="text" class="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Input your text">
 
+					
+				</div>
+				<!-- empty nput field error  -->
+				{#if inputError}
+				<p class="text-red-500">{inputError}</p>
+				{/if}
 
-<input onkeydown={addTodo} type="text" placeholder="Add todo" class="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-200 rounded-lg pl-5 pr-11 rtl:pr-5 rtl:pl-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mb-4" />
+			{#each todos as {text,id, status}}
+				<div   class=" inset-x-0 px-6 py-3 mx-5 mt-4 overflow-y-auto bg-white border rounded-md max-h-72 dark:bg-gray-900 dark:border-gray-700">
+					<a href={id.toString()} class="block py-1" >
+						<h3 class="font-medium text-gray-700 dark:text-gray-100 hover:underline">{text}</h3>
+						<p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Status: {status ? "done" : "pending"}</p>
+					</a>
+				</div>
+				{/each}
+			</section>
+		</div>
 
-
-<!-- <fieldset class="space-y-3">
-    <legend class="text-base font-semibold text-slate-900 dark:text-slate-200">Today</legend>
-    <div class="grid grid-cols-[1fr_24px] items-center gap-6">
-      <label class="peer grid grid-cols-[auto_1fr] items-center gap-3 rounded-md px-2 hover:bg-slate-100">
-        <input type="checkbox" class="peer size-3.5 appearance-none rounded-sm border border-slate-300 accent-pink-500 dark:accent-pink-600 checked:appearance-auto" checked="">
-        <span class="select-none text-slate-700 peer-checked:text-slate-400 peer-checked:line-through">Create a to do list</span>
-      </label>
-      <div class="size-[26px] rounded-md p-1 hover:bg-red-50 hover:text-red-500 peer-has-[:checked]:hidden">
-        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-      </div>
-    </div>
-    <div class="grid grid-cols-[1fr_24px] items-center gap-6">
-      <label class="peer grid grid-cols-[auto_1fr] items-center gap-3 rounded-md px-2 hover:bg-slate-100">
-        <input type="checkbox" class="peer size-3.5 appearance-none rounded-sm border border-slate-300 accent-pink-500 dark:accent-pink-600 checked:appearance-auto">
-        <span class="select-none text-slate-700 peer-checked:text-slate-400 peer-checked:line-through">Check off first item</span>
-      </label>
-      <div class="size-[26px] rounded-md p-1 hover:bg-red-50 hover:text-red-500 peer-has-[:checked]:hidden">
-        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-      </div>
-    </div>
-    <div class="grid grid-cols-[1fr_24px] items-center gap-6">
-      <label class="peer grid grid-cols-[auto_1fr] items-center gap-3 rounded-md px-2 hover:bg-slate-100">
-        <input type="checkbox" class="peer size-3.5 appearance-none rounded-sm border border-slate-300 accent-pink-500 dark:accent-pink-600 checked:appearance-auto">
-        <span class="select-none text-slate-700 peer-checked:text-slate-400 peer-checked:line-through">Investigate race condition</span>
-      </label>
-      <div class="size-[26px] rounded-md p-1 hover:bg-red-50 hover:text-red-500 block peer-has-[:checked]:hidden">
-        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-      </div>
-    </div>
-  </fieldset> -->
-<div class="space-y-4">
-
-	{#each filteredTodos as todo, i}
-	<div class="bg-slate-200 ">
-		<input oninput={editTodo} data-index = {i} type='text' value={todo.text} class="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-200 rounded-lg pl-5 pr-11 rtl:pr-5 rtl:pl-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 relative">
-		
-		<input onchange={toggleTodo} data-index = {i} type='checkbox' value={todo.done} class="absolute right-0 focus:outline-none rtl:left-0 rtl:right-auto">
 	</div>
-	
-	{/each}
-</div>
-	
-
-	<div class="space-x-2">
-	{#each ['all', 'active', 'completed'] as filterItem, i}
-	<button onclick={() => setFilter(filterItem)} class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-	{filterItem}
-	</button>
-	{/each}
-</div>
-
-{remaining()}
+	</div>
